06afd411ae8a1b4d06dfcb58e87d9ce1915a77a5
Hekkert, Erwin <erwin.hekkert@tauw.com>
2024-08-12 14:27:59 +0200

    added items to cart

---
+++ src/components/CartItem/index.jsx
@@ -0,0 +1,15 @@
+import './index.scss';
+
+const CartItem = ({cartItem: { name, price, imageUrl, quantity } }) => {
+    return (
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={`${name}`} />
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>{quantity} x €{price}</span>
+            </div>
+        </div>
+    )
+};
+
+export default CartItem;
